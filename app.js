3196dc0ae5f09f8768e7d64471d2da7c65922435
MatheusRezendev <contatorzn@hotmail.com>
2026-09-23 20:20:05 -0300

    feat: add DevSecOps checks to pipeline
    - SCA with npm audit (fails on high/critical)
    - Split Docker build and push, with Trivy scan in between
    - Update message to "Ola mundo DevSecOps!"
    - Fix qs vulnerability via npm audit fix
    - Ignore actions-runner/ in Jest and Docker context

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Ola mundo DevOps!");
+  res.status(200).send("Ola mundo DevSecOps!");
 });
 
 module.exports = app;
